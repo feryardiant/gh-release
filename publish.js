0fc60602e9fefed0ab4e2ed9418a49c1e0ef4df2
Fery Wardiyanto <ferywardiyanto@gmail.com>
2020-06-14 00:30:43 +0700

    feat(release): fetch release body from CHANGELOG.md file
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- publish.js
+++ publish.js
@@ -1,25 +1,23 @@
-const changelog = require('conventional-changelog')
 const pkg = require('./package')
+const { readFileSync } = require('fs')
 
-module.exports = new Promise((resolve, reject) => {
-    const stream = changelog({
-        preset: 'angular',
-        tagPrefix: 'v'
-    }, { version: pkg.version })
-    
-    stream.on('error', (err) => {
-        return reject(err)
+const readChangelogFile = async (version) => {
+    const changelog = readFileSync('CHANGELOG.md').toString()
+    const changes = changelog.split('\n')
+
+    const [curr, prev] = changes.filter(line => {
+        return /^(###?).(?<version>\[?[0-9]\.[0-9]\.[0-9]\]?).\((.*)\)$/.test(line)
+    }).slice(0, 2).map(line => {
+        const index = changes.findIndex(text => text === line)
+        const match = line.match(/\((?<link>https?\:.*)\)./)
+
+        return { index, line, compare: match ? match.groups.link : null }
     })
-    
-    let content = ''
-    stream.on('data', (buff) => {
-        content += buff.toString()
-    })
-    
-    stream.on('end', () => {
-        const changes = content.split('\n').slice(1).join('\n')
-        console.info(`\n---\n${changes.trim()}\n---\n`)
-        return resolve();
-    })
-})
+
+    console.info(changes.slice(curr.index + 1, prev.index).join('\n').trim())
+    console.info('\n---\n')
+    console.info('Compare with previous version:', curr.compare)
+}
+
+module.exports = readChangelogFile(pkg.version)
 
